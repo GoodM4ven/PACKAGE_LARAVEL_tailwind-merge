f8ce9739d5275c5aec5beb9bfd1c1a3bff209c6a
GoodM4ven <goodm4ven@proton.me>
2025-12-23 07:10:47 +0300

    WIP - Still trying for CI

--- workbench/routes/web.php
+++ workbench/routes/web.php
@@ -4,4 +4,19 @@ declare(strict_types=1);
 
 use Illuminate\Support\Facades\Route;
 
+Route::get('/build/{path}', function (string $path) {
+    $buildDirectory = realpath(base_path('workbench/public/build'));
+    $assetPath = realpath(base_path('workbench/public/build/'.ltrim($path, '/')));
+
+    if ($buildDirectory === false || $assetPath === false) {
+        abort(404);
+    }
+
+    if (! str_starts_with($assetPath, $buildDirectory.DIRECTORY_SEPARATOR) || ! is_file($assetPath)) {
+        abort(404);
+    }
+
+    return response()->file($assetPath);
+})->where('path', '.*');
+
 Route::view('/', 'demo');
